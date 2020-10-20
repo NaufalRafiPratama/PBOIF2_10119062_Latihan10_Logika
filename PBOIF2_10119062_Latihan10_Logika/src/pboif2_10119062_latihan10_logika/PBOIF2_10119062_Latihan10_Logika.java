4ef6bb3e09a27d187a3b8a83824f88c0a0267709
NaufalRafiPratama <73165365+NaufalRafiPratama@users.noreply.github.com>
2020-10-20 17:26:41 +0700

    Add files via upload

---
+++ PBOIF2_10119062_Latihan10_Logika/src/pboif2_10119062_latihan10_logika/PBOIF2_10119062_Latihan10_Logika.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2_10119062_latihan10_logika;
+
+/**
+ *
+ * @author 
+ * NAMA     : Naufal Rafi Pratama
+ * KELAS    : PBOIF2
+ * NIM      : 10119062
+ * Deskripsi Program : Program ini berisi program untuk menampilkan jenis-jenis
+ * tipe data bilangan bulat 
+ */
+public class PBOIF2_10119062_Latihan10_Logika {
+
+    public static void main(String[] args) {
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+    
+}
